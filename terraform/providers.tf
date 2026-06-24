bfbd42c5b5a2366472b788d01db009f77580823b
Yogesh Bharambe <yogesh01bharambe@gmail.com>
2026-06-24 13:46:01 +0530

    Adding helm code to project & updating AZ for EKS

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -13,7 +13,7 @@ locals {
   region          = "us-west-1"
   name            = "my-eks-cluster"
   vpc_cidr        = "10.0.0.0/16"
-  azs             = ["us-west-1a", "us-west-1b"]
+  azs             = ["us-west-1a", "us-west-1c"]
   public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
   private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
   intra_subnets   = ["10.0.5.0/24", "10.0.6.0/24"]
